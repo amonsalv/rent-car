b125ddb208463c4100038595f3ce4407e6981b36
amonsalv <anamonsalve96@gmail.com>
2023-06-03 15:07:14 -0500

    creacion de login screen y register para la app

--- App.js
+++ App.js
@@ -2,37 +2,118 @@
 //LIBRERIAS PARA EL MANEJO DE L NAVEGACION ENTRE PANTALLAS
 import { NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//
 
-//
-import  HomeScreen  from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import ContactsScreen from './screens/Contacts'
+import { Image } from "react-native";
 
-import Login from './screens/Login';
-import { View } from 'react-native';
+//screens
+import LoginScreen from "./screens/LoginScreen";
+import Register from "./screens/RegisterScreen";
+import Car from "./screens/CarScreen";
+import ListOfCars from "./screens/ListOfCars";
+import RentCar from "./screens/RentScreen";
 
-//DEFINIR LAS RUTAS DE CADA COMPONENTE
- const Stack = createNativeStackNavigator(); 
+//import User from './screens/HomeScreen';
+//import Rent from './screens/Rent';
+
+//import HomeScreen from "./screens/HomeScreen";
+import HomeTabs from "./screens/HomeTabs";
+
+/*function HeaderLogo() {
+  return (
+    <div style={{ width: '100%', maxWidth: '100%', height: 'auto', marginTop: '2rem', marginLeft: 'auto', marginRight: 'auto' }}>
+      <img source={require("./assets/car-rent.png")} alt="Banner" style={{ width: '100%', height: 'auto' }} />
+    </div>
+  );
+}*/
+
+
+function HeaderLogo() {
+  return (
+    <Image
+      source={require("./assets/car-rent.png")}
+      style={{ width: 380, height: 70, marginLeft:-20}}
+    />
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+/*
+const getIsSignedIn = () => {
+  // custom logic
+  return true;
+}*/
 
 export default function App() {
+  //const isSignedIn = getIsSignedIn();
 
   return (
-
     <NavigationContainer>
-      
-      <Stack.Navigator initialRouteName='Login'>
-      
-      <Stack.Screen name='Login' component={Login} options={{title:'Login'}}/>
+      <Stack.Navigator initialRouteName="HomeTabs">
+      {/*<Stack.Screen name="HomeTabs" component={HomeTabs} />*/}  
+        <Stack.Screen
+          name="HomeTabs"
+          component={HomeTabs}
+          options={{
+            headerTitle: () => <HeaderLogo />,
+           // headerTitleAlign: "",
+            headerStyle: {
+              backgroundColor: "#FFF",
+            },
+          }}
+        />
 
-        <Stack.Screen name='Profile' component={ProfileScreen} options={{title:'Perfil del Usuario'}}/>
-        <Stack.Screen name='Contacts' component={ContactsScreen} options={{title:'Contactenos'}}/>
-        <Stack.Screen name='Home' component={HomeScreen} options={{title:'Inicio'}}/>
-
+        {/*  {isSignedIn ? (
+          <>
+            <Stack.Screen name="Home" component={User} />
+            <Stack.Screen name="Profile" component={Car} />
+            <Stack.Screen name="Settings" component={Rent} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="SignIn" component={User} />
+            <Stack.Screen name="SignUp" component={User} />
+          </>
+        )} */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+/*
+function UserScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function CarScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Selecciona tu carro</Text>
+    </View>
+  );
+}
+
+function RentScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Renta carro</Text>
+    </View>
+  );
+}
 
 
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="User" component={User} />
+      <Tab.Screen name="Car" component={Car} />
+      <Tab.Screen name="Rent" component={Rent} />
+    </Tab.Navigator>
+  );
+}
+
+*/
